6d40f27d6cc7eed0f0c8dcbe1696cfe54efeefb5
SANGMIN <kwk680037@gmail.com>
2023-11-05 19:01:27 +0900

    💄 Add Project Section Fade Effect and Technologies Label (프로젝트 섹션 페이드효과, 기술라벨 추가)

--- src/components/Project/ProjectSection.js
+++ src/components/Project/ProjectSection.js
@@ -1,4 +1,39 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+import Form from "react-bootstrap/Form";
+
+const technologiesUsed = `
+    font-family: 'Noto Sans KR', sans-serif;
+    font-size: 13px;
+    font-weight: bold;
+
+    border-radius: 4px;
+    
+    margin-left: 0px;
+    padding-left: 5px;
+    padding-right: 5px; 
+`
+
+export const fadeIn1 = keyframes`
+    from {
+        opacity: 0;
+        transform: translateX(-35px);
+    }
+    to {
+        opacity: 1;
+        transform: translateX(0);
+    }
+`;
+
+export const fadeIn2 = keyframes`
+    from {
+        opacity: 0;
+        transform: translateX(35px);
+    }
+    to {
+        opacity: 1;
+        transform: translateX(0);
+    }
+`;
 
 export const ProjectSection = styled.div`
     margin-top: 10vh;
@@ -20,10 +55,18 @@ export const ProjectImg = styled.img`
     height: 30vh;
     border: 0.5px solid lightgray;
     box-shadow: -2px 10px 30px rgba(0, 0, 0, 0.2);
+
+    opacity: ${({ fadeLeft }) => (fadeLeft ? 1 : 0)};
+    transition: opacity 0.5s ease-in-out;
+    animation: ${({ fadeLeft }) => (fadeLeft ? fadeIn1 : 'none')} 1s ease-in-out;
 `
 
 export const ProjectDetail = styled.div`
     margin-left: 5vw;
+
+    opacity: ${({ fadeRight }) => (fadeRight ? 1 : 0)};
+    transition: opacity 0.5s ease-in-out;
+    animation: ${({ fadeRight }) => (fadeRight ? fadeIn2 : 'none')} 1s ease-in-out;
 `
 
 export const ProjectName = styled.h3`
@@ -33,4 +76,33 @@ export const ProjectName = styled.h3`
 
 export const ProjectMemo = styled.p`
     font-family: 'Orbit', sans-serif;
+    font-size: 16px;
+    margin-bottom: 1vh;
+`
+
+export const ReactJs = styled(Form.Label)`
+    ${technologiesUsed}
+    background-color: gray;
+    color: white
+`
+
+export const Bootstrap = styled(Form.Label)`
+    ${technologiesUsed}
+    background-color: purple;
+    color: white;
+    margin-left: 0.5vw;
+`
+
+export const Express = styled(Form.Label)`
+    ${technologiesUsed}
+    background-color: blue;
+    color: white;
+    margin-left: 0.5vw;
+`
+
+export const MySQL = styled(Form.Label)`
+    ${technologiesUsed}
+    background-color: Green;
+    color: white;
+    margin-left: 0.5vw;
 `
--- src/pages/mainPage/index.js
+++ src/pages/mainPage/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { AboutContainer, AboutSection, Introduce, ProfileImg, SubIntroduce } from "../../components/About/AboutSection";
-import { ProjectContainer, ProjectDetail, ProjectImg, ProjectMemo, ProjectName, ProjectSection, ProjectTitle } from "../../components/Project/ProjectSection";
+import { ReactJs, Bootstrap, Express, MySQL, ProjectContainer, ProjectDetail, ProjectImg, ProjectMemo, ProjectName, ProjectSection, ProjectTitle } from "../../components/Project/ProjectSection";
 
 import { useScroll } from "../../hooks/useScroll";
 
@@ -13,13 +13,18 @@ import ProjectImage from "../../image/projectImage.jpg";
 export const Index = () => {
     const [introduce, setIntroduce] = useState(false);
     const [profile, setProfile] = useState(false);
+    const [projectImg, setProjectImg] = useState(false);
+    const [projectDetail, setProjectDetail] = useState(false);
 
     useScroll((scrollY) => {
         const threshold = 300;
         const threshold2 = 500;
+        const threshold3 = 800;
 
         setProfile(scrollY > threshold);
         setIntroduce(scrollY > threshold2);
+        setProjectImg(scrollY > threshold3);
+        setProjectDetail(scrollY > threshold3);
     });
 
     return(
@@ -41,8 +46,8 @@ export const Index = () => {
             <ProjectTitle>Project</ProjectTitle>
 
             <ProjectContainer>
-                <ProjectImg src={ProjectImage}/>
-                <ProjectDetail>
+                <ProjectImg fadeLeft={projectImg} src={ProjectImage}/>
+                <ProjectDetail fadeRight={projectDetail}>
                     <ProjectName>
                         BookStock 프로젝트
                     </ProjectName>
@@ -51,9 +56,26 @@ export const Index = () => {
                         리버스 옥션(역경매) 기반 도서 구매 사이트 <br />
                         리버스 옥션이란? <br />
                         수요자가 원하는 물건에 대해 공급자 여러명이 <br />
-                        희망 판매 가격을 제시하며 최종 가격이 결정되는 서비스 <br />
-                        <Button className="mt-3" style={{fontWeight: "bold"}}>자세히보기 >></Button>
+                        희망 판매 가격을 제시하며 최종 가격이 결정되는 서비스
                     </ProjectMemo>
+
+                    <ReactJs>
+                        React
+                    </ReactJs>
+
+                    <Bootstrap>
+                        Bootstrap
+                    </Bootstrap>
+
+                    <Express>
+                        Express
+                    </Express>
+
+                    <MySQL>
+                        MySQL
+                    </MySQL> <br />
+
+                    <Button style={{fontWeight: "bold"}}>자세히보기 >></Button>
                 </ProjectDetail>
             </ProjectContainer>
         </ProjectSection>
