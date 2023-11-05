4306a596201fe5752ba1071c2c6900b5ca6d4316
SANGMIN <kwk680037@gmail.com>
2023-11-05 18:00:53 +0900

    💄 Add Project Section (프로젝트 섹션 추가)

--- src/components/Project/ProjectSection.js
+++ src/components/Project/ProjectSection.js
@@ -10,9 +10,27 @@ export const ProjectTitle = styled.h1`
     text-align: center;
 `
 
-export const ProjectImg = styled.img`
+export const ProjectContainer = styled.div`
+    display: flex;
     margin-top: 8vw;
-    margin-left: 10vw;
-    border: 1px solid black;
-    width: 25vw;
+    margin-left: 5vw;
+`
+export const ProjectImg = styled.img`
+    width: 45vw;
+    height: 30vh;
+    border: 0.5px solid lightgray;
+    box-shadow: -2px 10px 30px rgba(0, 0, 0, 0.2);
+`
+
+export const ProjectDetail = styled.div`
+    margin-left: 5vw;
+`
+
+export const ProjectName = styled.h3`
+    font-family: 'Orbit', sans-serif;
+    font-weight: bold
+`
+
+export const ProjectMemo = styled.p`
+    font-family: 'Orbit', sans-serif;
 `
--- src/pages/mainPage/index.js
+++ src/pages/mainPage/index.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { AboutContainer, AboutSection, Introduce, ProfileImg, SubIntroduce } from "../../components/About/AboutSection";
-import { ProjectImg, ProjectSection, ProjectTitle } from "../../components/Project/ProjectSection";
+import { ProjectContainer, ProjectDetail, ProjectImg, ProjectMemo, ProjectName, ProjectSection, ProjectTitle } from "../../components/Project/ProjectSection";
 
 import { useScroll } from "../../hooks/useScroll";
 
-import { Fade } from "react-bootstrap";
+import { Button, Fade } from "react-bootstrap";
 
 import ProfileImage from "../../image/IMG_8109.jpg";
-import ProjectImage from "../../image/logo2.jpg";
+import ProjectImage from "../../image/projectImage.jpg";
 
 
 export const Index = () => {
@@ -40,8 +40,25 @@ export const Index = () => {
         <ProjectSection>
             <ProjectTitle>Project</ProjectTitle>
 
-            <ProjectImg src={ProjectImage}/>
+            <ProjectContainer>
+                <ProjectImg src={ProjectImage}/>
+                <ProjectDetail>
+                    <ProjectName>
+                        BookStock 프로젝트
+                    </ProjectName>
+
+                    <ProjectMemo>
+                        리버스 옥션(역경매) 기반 도서 구매 사이트 <br />
+                        리버스 옥션이란? <br />
+                        수요자가 원하는 물건에 대해 공급자 여러명이 <br />
+                        희망 판매 가격을 제시하며 최종 가격이 결정되는 서비스 <br />
+                        <Button className="mt-3" style={{fontWeight: "bold"}}>자세히보기 >></Button>
+                    </ProjectMemo>
+                </ProjectDetail>
+            </ProjectContainer>
         </ProjectSection>
+
+        <div style={{height: "800px"}}></div>  {/* 나중에 지울것 */}
         </>
     );
 }
